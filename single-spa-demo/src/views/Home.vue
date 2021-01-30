6c700fa49975e6577c045daef3e3bae8dce6f6f1
wenhan <1196086345@qq.comc>
2021-01-31 01:40:09 +0800

    test

--- single-spa-demo/src/views/Home.vue
+++ single-spa-demo/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="container">
-        <h1>首页</h1>
+        <h1>首页1</h1>
         <!-- <router-link to="/portal">跳转</router-link> -->
     </div>
 </template>
@@ -9,9 +9,9 @@ import appConfig from '../../config/appConfig';
 export default {
     data(){
         return{
-      menu:appConfig
+      menu:appConfig,
     }
-    }
+    },
 }
 </script>
 <style scoped>
